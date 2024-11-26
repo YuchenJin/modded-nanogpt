a4ebc5f216249d18a29b6871e8b3b28e1c796579
YuchenJin <yuchenj@cs.washington.edu>
2024-11-26 15:27:51 +0000

    Change total steps to 16000.

--- train_gpt2.py
+++ train_gpt2.py
@@ -344,9 +344,9 @@ class Hyperparameters:
     batch_size : int = 8*128 # batch size, in sequences, across all devices
     device_batch_size : int = 8 # batch size, in sequences, per device
     sequence_length : int = 1024 # sequence length, in tokens
-    num_iterations : int = 12000 # number of iterations to run
+    num_iterations : int = 16000 # number of iterations to run
     warmup_iters : int = 0
-    warmdown_iters : int = 3428 # number of iterations of linear warmup/warmdown for triangular or trapezoidal schedule
+    warmdown_iters : int = 4571 # number of iterations of linear warmup/warmdown for triangular or trapezoidal schedule
     weight_decay : float = 0
     # evaluation and logging hyperparams
     val_loss_every : int = 125 # every how many steps to evaluate val loss? 0 for only at the end
